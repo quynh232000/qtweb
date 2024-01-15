0c8eb289f6513e4bb804b8c7426154db366e16e7
quynh232000 <quynh@gmail.com>
2024-01-15 21:30:53 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+  // const DB_HOST = 'localhost';
+  // const DB_USERNAME = 'root';
+  // const DB_PASSWORD = "";
+  // const DB_NAME = "dbtintonghop";
+  // const BASE_DIR = "/webtintonghop/";
+  // const PAGE_SIZE = 10;
+  // const NUM_RELATED_POST = 3;
+  // const NUM_POST_HOMEPAGE = 20;
+  const DB_HOST = 'localhost';
+  const DB_USERNAME = 'root';
+  const DB_PASSWORD = "123456";
+  const DB_NAME = "dbtintonghop";
+  const BASE_DIR = "/webtintonghop/";
+  const PAGE_SIZE = 10;
+  const NUM_RELATED_POST = 3;
+  const NUM_POST_HOMEPAGE = 20;
+  
+  
+  
+  
---
+++ site_info.php
@@ -0,0 +1,11 @@
+<?php
+   $site_info = [
+      'email' => 'admintintuchay@gmail.com',
+      'phone' => '09123123456',
+      'diachi'=>'123, cung Vàng, điện Ngọc, thành phố Trăng Vàng',
+      'tenAdmin' => 'Cao Thị Chót Vót',
+      'tenWebsite'=>'Tin tức tổng hợp',
+      'gioithieu'=>''
+   ]
+ ?>
+      
